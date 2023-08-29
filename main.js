0e8061b6f389c6c84288c52fd50acf07334bc62e
Ivan Cortes <ivancb97@gmail.com>
2023-08-29 12:39:57 -0300

    [prueba] Cada vez que se genera un nuevo carton lo llena con un numero, se saca la prueba del color

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // Seleccionar la columna
 
-const selectorColuma = () => {
+const selectorColumna = () => {
   return Math.floor(Math.random() * 9) + 1;
 };
 
 const llenarCasilla = (num_fila) => {
-  const columna = selectorColuma();
+  const columna = selectorColumna();
   const fila = document.querySelector(`.fila${num_fila}`);
   const casilla = fila.children[columna - 1];
   // Si la casilla no esta con background rojo, se llena. Sino se vuelve a intentar.
-  if (casilla.style.backgroundColor !== "red") {
-    casilla.style.backgroundColor = "red";
+  // if (casilla.style.backgroundColor !== "red") {
+  if (casilla.innerText === "") {
+    casilla.innerText = "num";
   } else {
     llenarCasilla(num_fila);
   }
@@ -18,34 +19,25 @@ const llenarCasilla = (num_fila) => {
 
   // Random depende num de columna
 };
-
-const prueba = document.getElementById("prueba");
-prueba.addEventListener("click", () => {
-  llenarCasilla(1);
-  llenarCasilla(2);
-  llenarCasilla(3);
-});
-
 const llenarCarton = () => {
   llenarCasilla(1);
   llenarCasilla(2);
   llenarCasilla(3);
-  console.log("Carton lleno");
 };
+
+// Boton nuevo carton
+
+let btnNuevoCarton = document.getElementById("nuevo-carton");
 
 // Vaciar carton
 
 const vaciarCarton = () => {
   const casillas = document.querySelectorAll(".celda");
   casillas.forEach((casilla) => {
-    casilla.style.backgroundColor = "white";
+    casilla.innerText = "";
   });
-  console.log("Carton vacio");
 };
 
-// Boton nuevo carton
-
-let btnNuevoCarton = document.getElementById("nuevo-carton");
 const nuevoCarton = () => {
   for (let i = 1; i <= 5; i++) {
     llenarCarton();
@@ -55,4 +47,5 @@ const nuevoCarton = () => {
 btnNuevoCarton.addEventListener("click", () => {
   vaciarCarton();
   nuevoCarton();
+  console.log("Nuevo carton generado");
 });
